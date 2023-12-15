b61045f9e6806812fc08a11e5a91a3968aac0ccf
exelCode226 <141802107+exelCode226@users.noreply.github.com>
2023-12-15 16:52:58 -0500

    Update actividades.routes.js

--- src/routes/actividades.routes.js
+++ src/routes/actividades.routes.js
@@ -3,11 +3,11 @@ const router = express.Router()
 import { authRequired, isAdminOPropietario, isAdministrador, isGerente, isGerenteOPropietario, isPropietario } from "../middlewares/validateToken.js";
 import { createActividad, deleteActividad, getActividad, getActividades, updateActividad } from "../controllers/actividades.controller.js";
 
-router.get("/actividades",authRequired, getActividades)
-router.post("/actividades",authRequired, createActividad)
-router.get("/actividades/:id",authRequired, getActividad)
-router.delete("/actividades/:id",authRequired, deleteActividad)
-router.put("/actividades/:id",authRequired, updateActividad)
+router.get("/actividades", getActividades)
+router.post("/actividades", createActividad)
+router.get("/actividades/:id",, getActividad)
+router.delete("/actividades/:id", deleteActividad)
+router.put("/actividades/:id", updateActividad)
 
 
 
